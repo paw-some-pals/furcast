5e16705004a3a59c1a7b47f63322535b54b644df
Heer-Perchani <heer.perchani@gmail.com>
2026-06-11 13:33:43 -0600

    made eda folder and acc_template for prompting

--- source/prompting_template_aac.py
+++ source/prompting_template_aac.py
@@ -11,46 +11,46 @@ load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
 
 # toy data - replace with your data!
-data = {
-    "Name": ["Alice", "Bob", "Charlie", "Diana"],
-    "Age": [25, 30, 35, 28],
-    "City": ["Edmonton", "Calgary", "Vancouver", "Toronto"],
-    "Is_Manager": [False, True, True, False]
-}
-df = pd.DataFrame(data)
+
+aac_df = pd.read_csv("datasets/acc_dog_cleaned.csv")
+breed_df = pd.read_csv("datasets/dog_breeds.csv")
 
 
 # define schema
 # 1. Define a strict child object instead of a Dict entry
-class CityClimateItem(BaseModel):
+class BreedItem(BaseModel):
     model_config = ConfigDict(populate_by_name=True) # Explicit constraint
-    city_name: str
-    climate_description: str
+    breed_1: str
+    breed2: str
+    mix: bool
 
 # 2. Main collection wrapper using a List shape
-class CityClimatesContainer(BaseModel):
+class BreedContainer(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
-    climates: list[CityClimateItem] # Safe structure (no hidden additionalProperties)
+    breed_classification: list[BreedItem] # Safe structure (no hidden additionalProperties)
 
+breed_list = breed_df["Name"].tolist()
+breed_string =  ", ".join(breed_list)
 
-# create the prompt
-base_string = "Given this list of cities, can you return a brief description of the climate of each city? "
-data_string = ", ".join(df['City'].tolist())
-print(data_string)
+for index in aac_df.index:
+    breed = aac_df.at[index,"breed"]
+    base_string = f"Given {breed}, classify it into breed1,breed2 and a boolean value(0 for false/1 for true) for if its a mix or not. If there is no second breed, use None as second breed. Use this {breed_string} to choose the proper breed. If it is not in here, choose the closest match."
 
-prompt_string = base_string + data_string
-print(prompt_string)
+    prompt_string = base_string
+    print(prompt_string)
 
-# prompt model
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=prompt_string,
-    config={
-        "response_mime_type": "application/json",
-        "response_schema": CityClimatesContainer, # Map class directly
-    },
-)
+    # prompt model
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt_string,
+        config={
+            "response_mime_type": "application/json",
+            "response_schema": BreedContainer, # Map class directly
+        },
+    )
+    if index == 10:
+        print(BreedContainer.model_validate_json(response.text))
+        print(response.text)
 
-# parse the model's response
-print(response.text)
-# print(CityClimatesContainer.model_validate_json(response.text))
+    # parse the model's response
+    
